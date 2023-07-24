ef063387ea92c498c125583bfbf266dd34dbfaca
Got-crypto <pecpalmplay@gmail.com>
2023-07-24 11:16:22 +0200

    added /api/fileanalyse' with its middleware to handle file uploads

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer')
+const bodyParser = require('body-parser')
 require('dotenv').config()
 
 var app = express();
+const { diskStorage } = multer
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({extended:!1}))
+
+const fileStorage = diskStorage({
+  destination: 'uploads',
+  filename: (req, file, cb) => {
+    cb(null, `${file.fieldname}_${Date.now()}` )
+  }
+})
+
+const fileUpload = multer({
+  storage: fileStorage,
+  limits: {
+    fieldSize: 1000000 * 50
+  },
+})
+
+app.use('/api/fileanalyse', fileUpload.any())
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
+
+app.post('/api/fileanalyse', (req, res) => {
+  const { originalname: name, mimetype: type, size } = req.files?.[0]
+
+  return res.json({name, type, size})
+})
 
 
 const port = process.env.PORT || 3000;
